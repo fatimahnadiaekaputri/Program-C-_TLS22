d83c1cca3723dfb03cc499ece3e8e3c67c5a37d7
fatimahnadiaekaputri <114746979+fatimahnadiaekaputri@users.noreply.github.com>
2022-09-30 20:05:51 +0700

    Add files via upload

---
+++ BMI Calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"BMI Calculator\n";
+    float massa, height, BMI;
+    
+    cout<<"\nMasukkan berat badan Anda dalam kilogram (kg) = ";
+    cin>>massa;
+    cout<<"\nMasukkan tinggi badan Anda dalam sentimeter (cm) = ";
+    cin>>height;
+    
+    
+    height=height/100;
+    BMI=massa/(height*height);
+    cout<<"\nBMI anda adalah= "<<BMI<<endl;
+    cout<<"\nKeterangan: ";
+    
+    if (BMI<18.5)
+    {
+        cout<<"\nBerat badan Anda kurang proporsional"<<endl;
+    }
+    else if (BMI>=18.5 && BMI<=22.9)
+    {
+        cout<<"\nBerat badan Anda normal"<<endl;
+    }
+    else if (BMI>=23 && BMI<=29.9)
+    {
+        cout<<"\nAnda memiliki berat badan berlebih dan berpotensi mengalami obesitas"<<endl;
+    }
+    else if (BMI>=30)
+    {
+        cout<<"\nAnda mengalami obesitas"<<endl;
+    }
+
+    return 0;
+}
